4c22103919810d74d430d482249451bfe1f5c66c
dengjunxiang <583214339@qq.com>
2017-12-03 10:12:07 +0800

    添加team模块交互

--- homePage/index.js
+++ homePage/index.js
@@ -1,3 +1,12 @@
 $(function() {
-    
+    var userList =eval(window.localStorage.getItem("users"));
+    $(".teamlist li").each(function (index, element){
+        var userId=userList[index].id;
+        var userPhone = userList[index].phone;
+        // console.log(userId,userPhone);
+        var teamTitle = this.children[1].children[0];
+        var teamCon = this.children[1].children[1];
+        $(teamTitle).html(userId);
+        $(teamCon).html(userPhone);
+    });
 })
